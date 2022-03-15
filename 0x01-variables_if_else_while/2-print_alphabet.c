268837101a0f3035cc1efd146b382d571d8877dc
Olatunde Success <olatundesuccess54@gmail.com>
2022-03-14 18:04:50 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /* this code help us to write small letter alphabet */
 int main (void)
+
 {
-    char A;
-    for (A='a'; A<='z'; A++)
-        putchar(A);
-    putchar('\n');
-    return(0);
+char A;
+for (A = 'a'; A <= 'z'; A++)
+putchar (A);
+putchar ('\n');
+return (0);
 }
